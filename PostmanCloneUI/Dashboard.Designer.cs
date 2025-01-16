29488ff053022e8903368d6cc86d46e3568462b7
[James <[jay.powellcs@gmail.com]>
2025-01-16 09:41:37 -0600

    Simple UI Design

--- PostmanCloneUI/Dashboard.Designer.cs
+++ PostmanCloneUI/Dashboard.Designer.cs
@@ -28,20 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            FormHeader = new Label();
+            apiLabel = new Label();
+            apitext = new TextBox();
+            callAPI = new Button();
+            resultstext = new TextBox();
+            statusStrip = new StatusStrip();
+            systemStatus = new ToolStripStatusLabel();
+            resultsLabel = new Label();
+            statusStrip.SuspendLayout();
             SuspendLayout();
+            // 
+            // FormHeader
+            // 
+            FormHeader.AutoSize = true;
+            FormHeader.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormHeader.Location = new Point(43, 43);
+            FormHeader.Name = "FormHeader";
+            FormHeader.Size = new Size(254, 47);
+            FormHeader.TabIndex = 0;
+            FormHeader.Text = "Postman Clone";
+            // 
+            // apiLabel
+            // 
+            apiLabel.AutoSize = true;
+            apiLabel.Location = new Point(43, 145);
+            apiLabel.Name = "apiLabel";
+            apiLabel.Size = new Size(60, 32);
+            apiLabel.TabIndex = 1;
+            apiLabel.Text = "API: ";
+            // 
+            // apitext
+            // 
+            apitext.BorderStyle = BorderStyle.FixedSingle;
+            apitext.Location = new Point(110, 138);
+            apitext.Name = "apitext";
+            apitext.Size = new Size(753, 39);
+            apitext.TabIndex = 2;
+            // 
+            // callAPI
+            // 
+            callAPI.BackColor = Color.DimGray;
+            callAPI.ForeColor = SystemColors.ButtonHighlight;
+            callAPI.Location = new Point(903, 138);
+            callAPI.Name = "callAPI";
+            callAPI.Size = new Size(139, 39);
+            callAPI.TabIndex = 3;
+            callAPI.Text = "Go";
+            callAPI.UseVisualStyleBackColor = false;
+            callAPI.Click += callAPI_Click;
+            // 
+            // resultstext
+            // 
+            resultstext.BackColor = SystemColors.ButtonHighlight;
+            resultstext.Location = new Point(43, 277);
+            resultstext.Multiline = true;
+            resultstext.Name = "resultstext";
+            resultstext.ReadOnly = true;
+            resultstext.ScrollBars = ScrollBars.Both;
+            resultstext.Size = new Size(999, 217);
+            resultstext.TabIndex = 4;
+            // 
+            // statusStrip
+            // 
+            statusStrip.Items.AddRange(new ToolStripItem[] { systemStatus });
+            statusStrip.Location = new Point(0, 607);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(1122, 35);
+            statusStrip.TabIndex = 5;
+            statusStrip.Text = "statusStrip1";
+            // 
+            // systemStatus
+            // 
+            systemStatus.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            systemStatus.Name = "systemStatus";
+            systemStatus.Size = new Size(69, 30);
+            systemStatus.Text = "Ready";
+            // 
+            // resultsLabel
+            // 
+            resultsLabel.AutoSize = true;
+            resultsLabel.Location = new Point(43, 233);
+            resultsLabel.Name = "resultsLabel";
+            resultsLabel.Size = new Size(88, 32);
+            resultsLabel.TabIndex = 6;
+            resultsLabel.Text = "Results";
             // 
             // Dashboard
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1486, 960);
+            AutoSize = true;
+            BackColor = Color.White;
+            ClientSize = new Size(1122, 642);
+            Controls.Add(resultsLabel);
+            Controls.Add(statusStrip);
+            Controls.Add(resultstext);
+            Controls.Add(callAPI);
+            Controls.Add(apitext);
+            Controls.Add(apiLabel);
+            Controls.Add(FormHeader);
             Font = new Font("Segoe UI", 18F);
-            Margin = new Padding(6, 6, 6, 6);
+            Margin = new Padding(6);
             Name = "Dashboard";
             Text = "Dashboard";
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label FormHeader;
+        private Label apiLabel;
+        private TextBox apitext;
+        private Button callAPI;
+        private TextBox resultstext;
+        private StatusStrip statusStrip;
+        private Label resultsLabel;
+        private ToolStripStatusLabel systemStatus;
     }
 }
--- PostmanCloneUI/Dashboard.cs
+++ PostmanCloneUI/Dashboard.cs
@@ -1,10 +1,34 @@
-namespace PostmanCloneUI
+namespace PostmanCloneUI;
+
+public partial class Dashboard : Form
 {
-    public partial class Dashboard : Form
+    public Dashboard()
     {
-        public Dashboard()
+        InitializeComponent();
+    }
+
+    private async void callAPI_Click(object sender, EventArgs e)
+    {
+
+        //Validate the API URL 
+
+
+        try
         {
-            InitializeComponent();
+            systemStatus.Text = "Calling API...";
+
+            //Sample Code - replace with the actual API Call 
+            await Task.Delay(2000);
+
+            systemStatus.Text = "Ready";
+        }
+        catch (Exception ex)
+        {
+
+            resultstext.Text = "Error: " + ex.Message;
+            systemStatus.Text = "Error";
         }
     }
+
+    
 }
