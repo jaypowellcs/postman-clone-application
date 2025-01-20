b7c2bcd2dee8af240bdff241833832776bf7c637
[James <[jay.powellcs@gmail.com]>
2025-01-20 11:10:25 -0600

    Post Changes

--- PostmanClone.Library/ApiAccess.cs
+++ PostmanClone.Library/ApiAccess.cs
@@ -11,13 +11,41 @@ namespace PostmanClone.Library
     {
 
         private readonly HttpClient client = new();
+
         public async Task<string> CallApiAsync(
             string url,
-            bool formatOutput = true,
-            HttpAction action = HttpAction.GET
+            string content,
+            HttpAction action = HttpAction.GET,
+            bool formatOutput = true
             )
         {
-            var response = await client.GetAsync(url);
+            StringContent stringContent = new(content, Encoding.UTF8, "application/json");
+            return await CallApiAsync( url, new StringContent( content), action, formatOutput );
+        }
+        public async Task<string> CallApiAsync(
+            string url,
+            HttpContent? content = null,
+            HttpAction action = HttpAction.GET,
+            bool formatOutput = true
+            )
+        {
+
+
+            HttpResponseMessage? response;
+
+            switch (action)
+            {
+                case HttpAction.GET:
+                    response = await client.GetAsync( url );
+                    break;
+                case HttpAction.POST:
+                    response = await client.PostAsync(url, content);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(action), action, null);
+            }
+
+            
 
             if (response.IsSuccessStatusCode)
             {
